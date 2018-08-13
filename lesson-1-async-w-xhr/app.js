a8b833ba9d5df2e20042fa5e1f01bcac307a55c2
Harold Beckerman <halbeckerman@gmail.com>
2018-08-13 01:52:24 -0500

    image request functioning

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -3,10 +3,40 @@
     const searchField = document.querySelector('#search-keyword');
     let searchedForText;
     const responseContainer = document.querySelector('#response-container');
+    const unsplashRequest = new XMLHttpRequest();
+    unsplashRequest.onload = addImage;
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        unsplashRequest.setRequestHeader('Authorization', 'Client-ID b661635622605a3f29a7682c4fa5087278bdbdd047404406831afd3b4e690ebe');
+        unsplashRequest.send();
     });
+
+
+    // unsplashRequest.onerror = function (err) {
+    //     requestError(err, 'image');
+    // };
+
+
+    function addImage() {
+
+        let htmlContent = '';
+        const data = JSON.parse(this.responseText);
+        const firstImage = data.results[0];
+
+        if (data && data.results && data.results[0]) {
+            htmlContent = `<figure>
+        <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+          <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+      </figure>`;
+        } else {
+            htmlContent = '<div class="error-no-image">No images available</div>';
+        }
+
+
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
 })();
