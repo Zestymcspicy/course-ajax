ffa7e78c44cc955ee6e718ffb24d332c3dfb8c98
Harold Beckerman <halbeckerman@gmail.com>
2018-08-14 00:55:37 -0500

    figured out how to post article headline

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -14,7 +14,7 @@
         searchedForText = searchField.value;
         unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
         unsplashRequest.setRequestHeader('Authorization', 'Client-ID b661635622605a3f29a7682c4fa5087278bdbdd047404406831afd3b4e690ebe');
-        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=`);
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=a1b691f0fa6b44d7adc63781747d5271`);
         articleRequest.send();
         unsplashRequest.send();
     });
@@ -24,10 +24,10 @@
     //     requestError(err, 'image');
     // };
 
+    let htmlContent = '';
 
     function addImage() {
 
-        let htmlContent = '';
         const data = JSON.parse(this.responseText);
         const firstImage = data.results[0];
 
@@ -41,8 +41,18 @@
         }
 
 
+    }
+    function addArticles() {
+        const articles = JSON.parse(this.responseText);
+        const firstArticle = articles.response.docs[0];
+
+        if (articles) {
+            htmlContent += `<div>${firstArticle.headline.main}</div>`;
+        } else {
+            htmlContent += `<p>Your search returned no articles</p>`;
+        }
+
         responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
     }
-    function addArticles () {}
 
 })();
