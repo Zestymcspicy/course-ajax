ac89e49b51f6d41b70dae09e3f9af9bad14f1295
Harold Beckerman <halbeckerman@gmail.com>
2018-08-15 02:05:44 -0500

    Added articles, headlines, snippets, randomized the image still needs error handling

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -10,5 +10,32 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+        $.ajax({
+            url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+            headers: {Authorization: 'Client-ID b661635622605a3f29a7682c4fa5087278bdbdd047404406831afd3b4e690ebe'}
+        }).done(addImage);
+        $.ajax({
+            url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=a1b691f0fa6b44d7adc63781747d5271`
+        }).done(addArticles);
     });
+
+    function addImage(images) {
+        const firstImage = images.results[(Math.floor(Math.random() * images.results.length))];
+
+        responseContainer.insertAdjacentHTML('afterbegin', `<figure>
+        <img src="${firstImage.urls.small}" alt="${searchedForText}">
+        <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+      </figure>)`
+        );
+    }
+    function addArticles(articles) {
+
+        responseContainer.insertAdjacentHTML('beforeend', '<ul>' +
+      articles.response.docs.map(article => `<li>
+        <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+        ${article.snippet}
+      </li>`) + '</ul>');
+    }
+
+
 })();
